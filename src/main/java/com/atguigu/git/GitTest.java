639a425829f244bbcc4059b6dac6c27705a192de
hzy <74344369+Tony-sir@users.noreply.github.com>
2022-07-14 15:09:00 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,6 +2,8 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello gitHub");
+        System.out.println("hello gitee");
+
     }
 }
